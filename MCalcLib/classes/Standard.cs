866f22e18dc1e82f6c0b5b9831746b5d1ce99403
Pavel Ferdman <p.ferdman@anc.net>
2016-03-11 19:42:50 +0200

    Fix Standard.cs issue

---
+++ MCalcLib/classes/Standard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCalcLib.classes
+{
+    public class Standard
+    {
+        /// <summary>
+        /// Название ГОСТа
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание ГОСТа
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// таблица габаритных свойств
+        /// </summary>
+        public Hashtable BoundProperties;
+
+        /// <summary>
+        /// Вес 1м погонного
+        /// </summary>
+        public double DensityWeight { get; set; }
+        
+    }
+}
