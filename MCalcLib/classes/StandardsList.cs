ebe2da0278ccca3889dbe99fd694e5488ee6501a
Павел Фердман <ferdman.p@gmail.com>
2016-04-05 22:54:21 +0300

    20160405H

--- MCalcLib/classes/StandardsList.cs
+++ MCalcLib/classes/StandardsList.cs
@@ -15,18 +15,25 @@ namespace MCalcLib.classes
         int index = -1;
         int currentArraySize = 100;
         const int arrayGrowSize = 100;
+        int counter = 0;
 
         public  int Length { get {return index;} }
         public  int Capacity {get {return currentArraySize;} }
+
+        public int Count()
+        {
+            return index + 1;
+        }
 
         public int Add(Standard standard)
         {
 
             if (index == standards.Length - 1)
-                reallocArray();
+                expandArray();
 
             standards[++index] = standard;
 
+            counter++;
             return index;
         }
 
@@ -36,6 +43,7 @@ namespace MCalcLib.classes
                 throw new IndexOutOfRangeException();
 
             standards[index] = standard;
+            counter++;
             return index;
         }
 
@@ -44,7 +52,15 @@ namespace MCalcLib.classes
             if (index == this.index)
                 RemoveLast();
             else
+            {
                 standards[index] = null;
+                reindexArray();
+            }
+            
+        }
+
+        private void reindexArray()
+        {
             
         }
 
@@ -54,7 +70,7 @@ namespace MCalcLib.classes
             index--;
         }
 
-        private void reallocArray()
+        private void expandArray()
         {
             currentArraySize+=arrayGrowSize;
             Standard[] locStandards = new Standard[currentArraySize];
---
+++ MCalcLibTests/classes/StandardsListTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MCalcLib.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCalcLib.classes.Tests
+{
+    [TestClass()]
+    public class StandardsListTests
+    {
+        bool inited = false;
+        StandardsList list = new StandardsList();
+        
+
+        [TestMethod()]
+        public void RemoveLastTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveLastTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MoveNextTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
